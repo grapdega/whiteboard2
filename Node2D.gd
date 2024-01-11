1f41e5a83c12e99f4e02793e07c3fc966eae4def
sulincix <parduscix@yandex.ru>
2024-01-11 19:49:18 +0000

    multi touch support

--- Node2D.gd
+++ Node2D.gd
@@ -7,6 +7,7 @@ func _ready():
 
 var points = []
 var lines = []
+var fingers = {}
 
 var mouse_enable = false
 func _input(event):
@@ -15,6 +16,14 @@ func _input(event):
 		if not mouse_enable:
 			lines.append(points)
 			points = []
+	if event is InputEventScreenTouch:
+		if event.index not in fingers:
+			fingers[event.index] = []
+		if not event.pressed:
+			lines.append(fingers[event.index])
+			fingers[event.index] = []
+	if event is InputEventScreenDrag:
+		fingers[event.index].append(Vector2(event.position.x, event.position.y))
 	if event is InputEventMouseMotion:
 		if mouse_enable:
 			points.append(Vector2(get_global_mouse_position().x, get_local_mouse_position().y))
@@ -23,7 +32,10 @@ func _process(delta):
 	queue_redraw()
 
 func _draw():
-	for pnts in lines  + [points]:
+	var fpnts = []
+	for key in fingers.keys():
+		fpnts.append(fingers[key])
+	for pnts in lines  + [points] + fpnts:
 		var i=1
 		while i < pnts.size()-1:
 			draw_line(pnts[i-1],pnts[i],Color.BLUE, 3, true)
